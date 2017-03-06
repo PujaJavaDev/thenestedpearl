92f04ab99023864bd959e894ddde81b966707030
Puja Maheshwari <pujamaheshwari@gmail.com>
2017-03-06 13:47:56 -0500

    Added initial project

---
+++ src/main/java/com/thenestedpearl/ThenestedpearlApplication.java
@@ -0,0 +1,12 @@
+package com.thenestedpearl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThenestedpearlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThenestedpearlApplication.class, args);
+	}
+}
