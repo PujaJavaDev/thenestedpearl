5aded93462651f8c72125b516c378ec661183750
Puja Maheshwari <pujamaheshwari@gmail.com>
2017-03-06 16:32:39 -0500

    Added logging setup with logback

--- src/test/java/com/thenestedpearl/ThenestedpearlApplicationTests.java
+++ src/test/java/com/thenestedpearl/ThenestedpearlApplicationTests.java
@@ -1,16 +1,28 @@
 package com.thenestedpearl;
 
+
+import com.thenestedpearl.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = ThenestedpearlApplication.class)
+@WebAppConfiguration
 public class ThenestedpearlApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private I18NService i18NService;
 
+	@Test
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
+	}
 }
