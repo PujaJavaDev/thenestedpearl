cf6085ea9a941fe4cb5e054512d4c24c6dde7928
Puja Maheshwari <pujamaheshwari@gmail.com>
2017-03-07 22:22:37 -0500

    Added payload page

---
+++ src/main/java/com/thenestedpearl/web/controllers/PayloadController.java
@@ -0,0 +1,18 @@
+package com.thenestedpearl.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by omsairam on 2/28/2017.
+ */
+@Controller
+public class PayloadController {
+
+    public static final String PAYLOAD_VIEW_NAME = "payload/payload";
+
+    @RequestMapping("/payload")
+    public String payload() {
+        return PAYLOAD_VIEW_NAME;
+    }
+}
